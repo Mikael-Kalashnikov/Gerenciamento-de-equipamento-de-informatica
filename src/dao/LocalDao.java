f15957ab9d80dd8ef232537354024d79976f561a
Afonso017 <afonsosd016@gmail.com>
2023-09-09 23:02:29 -0300

    Att: LocalDao

--- src/dao/LocalDao.java
+++ src/dao/LocalDao.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.sql.*;
 import model.entity.Local;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocalDao extends BaseDaoImp<Local> {
@@ -26,12 +28,78 @@ public class LocalDao extends BaseDaoImp<Local> {
 		return null;
 	}
 	
-	public void deletar(Local loc){}
-	public void alterar(Local loc){}
-	public Endereco buscar(Local loc){
-		return null;
+	public void deletar(Local loc){
+        Connection con = BaseDaoImp.getConnection();
+        String sql = "DELETE FROM local WHERE id_local = ?";
+        try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setLong(1, loc.getId());
+			ps.execute();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+            BaseDaoImp.closeConnection();
+		}
+    }
+	public void alterar(Local loc){
+        Connection con = BaseDaoImp.getConnection();
+        String sql = "UPDATE local SET nome = ?, nomeCompartimento = ? WHERE id_local = ?";
+        try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, loc.getNome());
+            ps.setString(2, loc.getNomeCompartimento());
+            ps.setLong(3, loc.getId());
+			ps.execute();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+            BaseDaoImp.closeConnection();
+		}
+    }
+	public Local buscar(Local loc){
+		Connection con = BaseDaoImp.getConnection();
+        String sql = "SELECT * FROM local WHERE id_local = ?";
+        Local local = null;
+        try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setLong(1, loc.getId());
+			ps.execute();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                local = new Local();
+                local.setId(rs.getLong(1));
+                local.setNome(rs.getString(2));
+                local.setNomeCompartimento(rs.getString(3));
+            }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+            BaseDaoImp.closeConnection();
+		}
+		return local;
 	}
 	public List<Local> listar(){
-		return null;
+		Connection con = BaseDaoImp.getConnection();
+        String sql = "SELECT * FROM local";
+        List<Local> lista = new ArrayList<>();
+        try {
+			PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Local loc = new Local(rs.getLong(1), rs.getString(2), rs.getString(3));
+                lista.add(loc);
+            }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+            BaseDaoImp.closeConnection();
+		}
+		return lista;
 	}
 }
